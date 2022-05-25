9a921eed19da90f350fea8e1a0addbc0c69ec763
nikoniko00 <techcamp.mai.japan@gmail.com>
2022-05-25 22:23:24 +0900

    コードレビュー後修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_date, presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
